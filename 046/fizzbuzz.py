04f4004b31e89f61c6cc2b3996036d4a91926eab
Andrew Chung <590100+andrewchung@users.noreply.github.com>
2021-06-21 20:14:14 -0400

    submission Bite 46 @ codechalleng.es

--- 046/fizzbuzz.py
+++ 046/fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(num):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        return("Fizz Buzz")
+    elif num % 3 == 0:
         return("Fizz")
     elif num % 5 == 0:
         return("Buzz")
